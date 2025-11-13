7d9f9c6796eb575b65e0899885b1292eb0e8c07c
RayhanWahyu9 <rayhanwahyu9@gmail.com>
2025-11-13 13:56:21 +0700

    update lagi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1782375933"
+    serviceWorkerVersion: "2425631920"
   }
 });
